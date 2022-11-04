5a4410590f4c4292d207c3279c4596ec2b6e6985
es9dpw <es9dpw999@gmail.com>
2022-11-04 11:28:43 +0000

    Second commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
-        System.out.println("1. Add Song");
-        System.out.println("2. Remove Song");
-        System.out.println("3. List Songs");
-        System.out.println("4. List Top Song");
-        System.out.println("5. Exit Program");
+        boolean exit = false;
+        while (exit == false);
+            System.out.println("MAIN MENU");
+            System.out.println("");
+            System.out.println("1. Add Song");
+            System.out.println("2. Remove Song");
+            System.out.println("3. List Songs");
+            System.out.println("4. List Top Song");
+            System.out.println("5. Exit Program");
+            System.out.println("");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your choice: ");
+            String option = scanner.nextLine();
     }
 }
