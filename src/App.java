0a2ea5b62b2660c3a84cded5b9ad17a623e0ae3e
es9dpw <es9dpw999@gmail.com>
2022-11-04 09:04:38 +0000

    Fist commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("1. Add Song");
+        System.out.println("2. Remove Song");
+        System.out.println("3. List Songs");
+        System.out.println("4. List Top Song");
+        System.out.println("5. Exit Program");
+    }
+}
