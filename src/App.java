43d4930c4a9a4cd3d2d019be5ee997565e999fa9
es9dpw <es9dpw999@gmail.com>
2022-11-04 12:04:25 +0000

    4th Commit

--- src/App.java
+++ src/App.java
@@ -2,19 +2,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
+        String option = "null";
         boolean exit = false;
         while (exit == false){
-            System.out.println("MAIN MENU");
-            System.out.println("");
-            System.out.println("1. Add Song");
-            System.out.println("2. Remove Song");
-            System.out.println("3. List Songs");
-            System.out.println("4. List Top Song");
-            System.out.println("5. Exit Program");
-            System.out.println("");
+            System.out.print("MAIN MENU \n\n1. Add Song \n2. Remove Song \n3. List Songs \n4. List Top Song \n5. Exit Program \n\nPlease enter your choice: ");
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter your choice: ");
-            String option = scanner.nextLine();
+            option = scanner.next();
+            System.out.println("");
+            if (option.equals("1")){
+                System.out.println("you picked 1\n");
+            }
+            else if (option.equals("2")){
+                System.out.println("you picked 2\n");
+            }
+            else if (option.equals("3")){
+                System.out.println("you picked 3\n");
+            }
+            else if (option.equals("4")){
+                System.out.println("you picked 4\n");
+            }
+            else if (option.equals("5")){
+                System.out.println("Exiting Program");
+                exit = true;
+            }
+            else{
+                System.out.println("Invalid option, try again:\n");
+            }
         }
     }
 }
