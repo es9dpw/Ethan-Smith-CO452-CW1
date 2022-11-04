9e2d186f1ed0a57a0ea2a44702f483f1ee77fc2c
es9dpw <es9dpw999@gmail.com>
2022-11-04 11:29:50 +0000

    3rd commit

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         boolean exit = false;
-        while (exit == false);
+        while (exit == false){
             System.out.println("MAIN MENU");
             System.out.println("");
             System.out.println("1. Add Song");
@@ -15,5 +15,6 @@ public class App {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter your choice: ");
             String option = scanner.nextLine();
+        }
     }
 }
