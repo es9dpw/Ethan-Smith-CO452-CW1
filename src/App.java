6e24015588e993051aac6294d313dea5027db934
es9dpw <es9dpw999@gmail.com>
2022-11-07 10:34:31 +0000

    commit

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         boolean exit = false;
-        int songNumber = 0;
+        int songCount = -1;
         ArrayList<String> songList = new ArrayList<String>();
         ArrayList<String> artistList = new ArrayList<String>();
         ArrayList<String> playsList = new ArrayList<String>();
@@ -14,17 +14,48 @@ public class App {
             String option = scanner.next();
             System.out.println("");
             if (option.equals("1")){
-                songNumber = songNumber + 1;
                 System.out.print("Enter name of song: ");
                 songList.add(scanner.next());
                 System.out.print("Enter name of artist: ");
                 artistList.add(scanner.next());
                 System.out.print("Enter number of plays: ");
                 playsList.add(scanner.next());
+                songCount = songCount + 1;
                 System.out.println("Song Added\n");
             }
             else if (option.equals("2")){
-                System.out.println("you picked 2\n");
+                boolean found = false;
+                int songNumber = 0;
+                System.out.print("Enter name of song to remove: ");
+                System.out.println("");
+                String remove = scanner.next();
+                while (found == false){
+                    if (remove.equals(songList.get(songNumber))){
+                        songList.remove(songNumber);
+                        artistList.remove(songNumber);
+                        playsList.remove(songNumber);
+                        songCount = songCount - 1;
+                        found = true;
+                        System.out.println("Song Removed\n");
+                    }
+                    else if (songNumber > songCount){
+                        System.out.print("Song not found. If you wish to try again, type \"try again\" or press enter to return to the main menu: ");
+                        String tryAgain = scanner.next();
+                        if (tryAgain.equals("try again")){
+                            System.out.println("");
+                            System.out.print("Enter name of song to remove: ");
+                            remove = scanner.next();
+                            System.out.println("");
+                        }
+                        else{
+                            found = true;
+                            System.out.println("");
+                        }
+                    }
+                    else{
+                        songNumber = songNumber + 1;
+                    }
+                }
             }
             else if (option.equals("3")){
                 System.out.println("you picked 3\n");
