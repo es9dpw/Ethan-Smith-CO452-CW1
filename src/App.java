b76107c35dbc5bbb0a55b769c16adc24ce7a1cd1
es9dpw <es9dpw999@gmail.com>
2022-11-07 10:05:40 +0000

    commit

--- src/App.java
+++ src/App.java
@@ -1,16 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        String option = "null";
         boolean exit = false;
+        int songNumber = 0;
+        ArrayList<String> songList = new ArrayList<String>();
+        ArrayList<String> artistList = new ArrayList<String>();
+        ArrayList<String> playsList = new ArrayList<String>();
         while (exit == false){
             System.out.print("MAIN MENU \n\n1. Add Song \n2. Remove Song \n3. List Songs \n4. List Top Song \n5. Exit Program \n\nPlease enter your choice: ");
             Scanner scanner = new Scanner(System.in);
-            option = scanner.next();
+            String option = scanner.next();
             System.out.println("");
             if (option.equals("1")){
-                System.out.println("you picked 1\n");
+                songNumber = songNumber + 1;
+                System.out.print("Enter name of song: ");
+                songList.add(scanner.next());
+                System.out.print("Enter name of artist: ");
+                artistList.add(scanner.next());
+                System.out.print("Enter number of plays: ");
+                playsList.add(scanner.next());
+                System.out.println("Song Added\n");
             }
             else if (option.equals("2")){
                 System.out.println("you picked 2\n");
